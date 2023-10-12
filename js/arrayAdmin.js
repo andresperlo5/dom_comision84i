11a6a423fa500b26166342745cf3141c506eded1
unknown <andres_perlo@hotmail.com>
2023-10-12 15:19:02 -0300

    se modifica usuarios

--- js/arrayAdmin.js
+++ js/arrayAdmin.js
@@ -1,20 +1,24 @@
+const users = JSON.parse(localStorage.getItem("users")) || [];
+
 const admins = [
   {
     id: 1,
-    user: "andres2023",
-    pass: "123456789",
+    usuario: "andres2023",
+    contrasenia: "123456789",
     login: false,
     role: "admin",
     deleted: false,
   },
   {
     id: 2,
-    user: "maxi2023",
-    pass: "123456789",
+    usuario: "maxi2023",
+    contrasenia: "123456789",
     login: false,
     role: "admin",
     deleted: false,
   },
 ];
 
-localStorage.setItem("admin", JSON.stringify(admins));
+if (users.length === 0) {
+  localStorage.setItem("users", JSON.stringify(admins));
+}
